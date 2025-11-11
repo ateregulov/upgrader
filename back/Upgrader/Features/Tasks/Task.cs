1e8ff938fca9da858f1c22adbc9e70f95db46b55
CITRUSR <savelypetrov2007@mail.ru>
2025-11-11 13:59:29 +0300

    feat(Task): extend model with isCompleted and results props

--- back/Upgrader/Features/Tasks/Task.cs
+++ back/Upgrader/Features/Tasks/Task.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using Upgrader.Features.Courses;
 
 namespace Upgrader.Features.Tasks;
@@ -9,10 +10,14 @@ public class Task
     public Guid CourseId { get; set; }
     public Course Course { get; set; }
     public int Order { get; set; }
+    public List<TaskResult> Results { get; set; }
 
     [Required]
     public string Title { get; set; }
 
     [Required]
     public string Text { get; set; }
+
+    [NotMapped]
+    public bool IsCompeted { get; set; }
 }
