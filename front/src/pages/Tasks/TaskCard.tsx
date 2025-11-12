85dccb3fd49f85169d79dee554a3e424da3f74f2
CITRUSR <savelypetrov2007@mail.ru>
2025-11-12 13:49:54 +0300

    feat(taskCard): update completed style

--- front/src/pages/Tasks/TaskCard.tsx
+++ front/src/pages/Tasks/TaskCard.tsx
@@ -10,14 +10,14 @@ function TaskCard({ task, onClick }: TaskCardProps) {
     <div
       key={task.id}
       onClick={() => task.isUnlocked && onClick?.(task.id)}
-      className={`flex-shrink-0 w-64 p-4 rounded-lg transition-colors cursor-pointer
+      className={`flex flex-col flex-shrink-0 w-64 p-4 rounded-lg transition-colors cursor-pointer
         ${task.isUnlocked ? 'bg-gray-800 hover:bg-gray-700' : 'bg-gray-700 opacity-50 cursor-not-allowed'}`}
     >
       <h2 className='text-white font-semibold mb-2'>{task.title}</h2>
       <p className='text-gray-300 text-sm mb-2'>{task.text}</p>
 
       {task.isCompleted && (
-        <span className='inline-block mt-2 text-xs text-green-400 font-semibold'>
+        <span className='mt-auto text-xs text-green-400 font-semibold'>
           ✔ Пройдено
         </span>
       )}
