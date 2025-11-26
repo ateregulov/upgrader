4a365bfdd8afcbf732566f376c1fa0a4965a430f
CITRUSR <savelypetrov2007@mail.ru>
2025-11-26 22:46:57 +0300

    feat(courseAnalysesResultsController): add create result endpoint

--- back/Upgrader/Features/Courses/CourseAnalysesResultsController.cs
+++ back/Upgrader/Features/Courses/CourseAnalysesResultsController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using OrisAppBack.Features.Bot;
 using Upgrader.Auth;
 
 namespace Upgrader.Features.Courses;
@@ -9,10 +10,12 @@ namespace Upgrader.Features.Courses;
 public class CourseAnalysesResultsController : ControllerBase
 {
     private readonly MyContext _dbContext;
+    private readonly AppBot _appBot;
 
-    public CourseAnalysesResultsController(MyContext dbContext)
+    public CourseAnalysesResultsController(MyContext dbContext, AppBot appBot)
     {
         _dbContext = dbContext;
+        _appBot = appBot;
     }
 
     [HttpGet]
@@ -29,5 +32,39 @@ public class CourseAnalysesResultsController : ControllerBase
             .FirstOrDefaultAsync();
 
         return Ok(result);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateResult(CreateResultDto dto)
+    {
+        var headersData = await this.GetHeadersData();
+        if (headersData == null)
+            return Unauthorized();
+
+        var user = await _dbContext.Users.SingleOrDefaultAsync(x => x.TelegramId == headersData.TelegramId);
+
+        var request = await _dbContext.CourseAnalyzeRequests
+            .FirstOrDefaultAsync(x => x.Id == dto.RequestId);
+        if (request == null)
+            return NotFound();
+
+        var result = new CourseAnalyzeResult
+        {
+            RequestId = dto.RequestId,
+            Message = dto.Message,
+        };
+
+        await _dbContext.CourseAnalyzeResults.AddAsync(result);
+        await _dbContext.SaveChangesAsync();
+
+        await _appBot.SendMessageAsync(dto.Message, user.TelegramId.Value);
+
+        return Ok();
+    }
+
+    public class CreateResultDto
+    {
+        public Guid RequestId { get; set; }
+        public string Message { get; set; }
     }
 }
