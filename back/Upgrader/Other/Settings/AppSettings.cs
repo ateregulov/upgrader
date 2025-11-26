59e1e668dd2a8c0589d46653dbdded90a8a78cf9
CITRUSR <savelypetrov2007@mail.ru>
2025-11-26 15:37:15 +0300

    feat(appsettings): add CourseSettings

--- back/Upgrader/Other/Settings/AppSettings.cs
+++ back/Upgrader/Other/Settings/AppSettings.cs
@@ -5,11 +5,17 @@ namespace OrisAppBack.Other.Settings
         public string TelegramBotToken { get; set; }
         public string TelegramBotName { get; set; }
         public BonusSettings BonusSettings { get; set; }
+        public CourseSettings CourseSettings { get; set; }
     }
 
     public class BonusSettings
     {
         public decimal RegisterBonus { get; set; }
         public decimal ReferrerBonus { get; set; }
+    }
+
+    public class CourseSettings
+    {
+        public decimal AnalysisPrice { get; set; }
     }
 }
