932e1e8b7f1022140e382d2e0a4133ecc37a735e
CITRUSR <savelypetrov2007@mail.ru>
2025-11-27 20:21:17 +0300

    refactoring(CourseAnalysesController): replace logic with service

--- back/Upgrader/Features/Courses/CourseAnalysesController.cs
+++ back/Upgrader/Features/Courses/CourseAnalysesController.cs
@@ -1,11 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Options;
-using OrisAppBack.Other.Settings;
 using Upgrader.Auth;
-using Upgrader.Features.Balance;
-using Upgrader.Features.Tasks;
-using Upgrader.Features.Transactions;
 
 namespace Upgrader.Features.Courses;
 
@@ -13,26 +7,17 @@ namespace Upgrader.Features.Courses;
 [Route("api/course-analyses")]
 public class CourseAnalysesController : ControllerBase
 {
-    private readonly MyContext _dbContext;
-    private readonly TaskService _taskService;
-    private readonly BalanceService _balanceService;
-    private readonly TransactionService _transactionService;
-    private readonly decimal _analyzePrice;
+    private readonly CourseAnalyzeService _analyzeService;
 
-    public CourseAnalysesController(MyContext dbContext, BalanceService balanceService,
-        IOptions<AppSettings> appSettingsOpt, TransactionService transactionService, TaskService taskService)
+    public CourseAnalysesController(CourseAnalyzeService analyzeService)
     {
-        _dbContext = dbContext;
-        _balanceService = balanceService;
-        _analyzePrice = appSettingsOpt.Value.CourseSettings.AnalysisPrice;
-        _transactionService = transactionService;
-        _taskService = taskService;
+        _analyzeService = analyzeService;
     }
 
     [HttpGet("price")]
     public async Task<IActionResult> GetPrice()
     {
-        return Ok(_analyzePrice);
+        return Ok(_analyzeService.GetAnalyzePrice());
     }
 
     [HttpGet]
@@ -42,20 +27,15 @@ public class CourseAnalysesController : ControllerBase
         if (headersData == null)
             return Unauthorized();
 
-        var isCourseExists = await _dbContext.Courses.AnyAsync(x => x.Id == courseId);
-        if (!isCourseExists)
+        var requestResult = await _analyzeService.GetAsync(courseId, headersData.UserId, includeResult);
+        if (!requestResult.Succeeded)
         {
-            return NotFound("Курс не найден");
+            if (requestResult.ErrorsString.Contains("не найден"))
+                return NotFound(requestResult.ErrorsString);
+            return BadRequest(requestResult.ErrorsString);
         }
 
-        IQueryable<CourseAnalyzeRequest> requestQuery = _dbContext.CourseAnalyzeRequests;
-        if (includeResult)
-            requestQuery = requestQuery.Include(x => x.Result);
-
-        var request = await requestQuery
-            .FirstOrDefaultAsync(x => x.CourseId == courseId && x.UserId == headersData.UserId);
-
-        return Ok(request);
+        return Ok(requestResult.Data);
     }
 
     [HttpPost]
@@ -65,38 +45,14 @@ public class CourseAnalysesController : ControllerBase
         if (headersData == null)
             return Unauthorized();
 
-        var isCourseExists = await _dbContext.Courses.AnyAsync(x => x.Id == dto.CourseId);
-        if (!isCourseExists)
+        var result = await _analyzeService.CreateRequestAsync(dto.CourseId, headersData.UserId);
+
+        if (!result.Succeeded)
         {
-            return NotFound("Курс не найден");
+            if (result.ErrorsString.Contains("не найден"))
+                return NotFound(result.ErrorsString);
+            return BadRequest(result.ErrorsString);
         }
-
-        var tasks = await _taskService.GetTasksAsync(dto.CourseId, headersData.UserId);
-        if (!tasks.All(x => x.IsCompleted))
-        {
-            return BadRequest("Нельзя создать анализ курса не ответив на все задания");
-        }
-
-        var balance = await _balanceService.GetBalanceAsync(headersData.UserId);
-        if (balance < _analyzePrice)
-        {
-            return BadRequest("INFLUENT_BALANCE");
-        }
-
-        await _transactionService.CreateTransactionAsync(
-            _analyzePrice,
-            TransactionType.CourseAnalyze,
-            senderId: headersData.UserId,
-            uniqueKey: $"CourseAnalyze-{dto.CourseId}-{headersData.UserId}"
-        );
-
-        var courseAnalyzeRequest = new CourseAnalyzeRequest
-        {
-            CourseId = dto.CourseId,
-            UserId = headersData.UserId,
-        };
-        await _dbContext.CourseAnalyzeRequests.AddAsync(courseAnalyzeRequest);
-        await _dbContext.SaveChangesAsync();
 
         return Ok();
     }
